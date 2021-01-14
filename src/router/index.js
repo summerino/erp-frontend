4fabd75de8eeda9d91cba1639afb8cc83a0b5d1d
Rudy Wang <rudywang87@gmail.com>
2021-01-14 13:48:16 +0700

    Move BlankPage.vue inside views folder

--- src/router/index.js
+++ src/router/index.js
@@ -23,7 +23,7 @@ export const routes = [{
 {
   path: '/blank',
   name: 'blank',
-  component: () => import(/* webpackChunkName: "blank" */ '@/pages/BlankPage.vue'),
+  component: () => import(/* webpackChunkName: "blank" */ '@/views/BlankPage.vue'),
   meta: {
     authRequired: true
   }
