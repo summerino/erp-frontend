89b8610e68a45000c4a28464fef8df33daaa2c08
Rudy Wang <rudywang87@gmail.com>
2021-01-26 21:49:45 +0700

    Move item, item.category, item.group, uom, uom.conversion to db.json

--- src/fake-db/data/inventory/index.js
+++ src/fake-db/data/inventory/index.js
@@ -1,156 +1,22 @@
+import data from '@/fake-db/db.json'
 import mock from '@/fake-db/mock.js'
+import axios from 'axios'
+
 import itemCategory from './item.category'
 import itemGroup from './item.group'
 import uom from './uom.conversion'
-
-const data = {
-  items: [{
-    id: 1,
-    code: 'ABC',
-    name: 'AURORA BUTTER COOKIES',
-    description: '',
-    typeId: 6,
-    typeName: 'Resell',
-    qty: 100,
-    uomId: 1,
-    uomCode: 'DUS @ 6 KLG',
-    categoryId: 33,
-    categoryCode: 'F-S-B',
-    categoryName: 'Makanan - Snack - Biskuit',
-    uomSellId: 2,
-    uomSellName: 'DUS',
-    sellPrice: 243000.00,
-    uomBuyId: 2,
-    uomBuyName: 'DUS',
-    buyPrice: 210609.00
-  }, {
-    id: 2,
-    code: 'ABC120',
-    name: 'ATM BUTTER 120 CHOCOLATE',
-    description: '',
-    typeId: 6,
-    typeName: 'Resell',
-    qty: 100,
-    uomId: 2,
-    uomCode: 'DUS @ 6 PAK X 20 BKS',
-    categoryId: 33,
-    categoryCode: 'F-S-B',
-    categoryName: 'Makanan - Snack - Biskuit',
-    uomSellId: 5,
-    uomSellName: 'DUS',
-    sellPrice: 45000.00,
-    uomBuyId: 5,
-    uomBuyName: 'DUS',
-    buyPrice: 40800.00
-  }, {
-    id: 3,
-    code: 'ABC24',
-    name: 'ATM BUTTER 24 CHOCOLATE',
-    description: '',
-    typeId: 6,
-    typeName: 'Resell',
-    qty: 100,
-    uomId: 3,
-    uomCode: 'DUS @ 4 PAK X 6 BKS',
-    categoryId: 33,
-    categoryCode: 'F-S-B',
-    categoryName: 'Makanan - Snack - Biskuit',
-    uomSellId: 8,
-    uomSellName: 'DUS',
-    sellPrice: 63000.00,
-    uomBuyId: 8,
-    uomBuyName: 'DUS',
-    buyPrice: 60000.00
-  }, {
-    id: 4,
-    code: 'ABK',
-    name: 'ATM BISKUIT KACANG',
-    description: '',
-    typeId: 6,
-    typeName: 'Resell',
-    qty: 100,
-    uomId: 3,
-    uomCode: 'DUS @ 4 PAK X 6 BKS',
-    categoryId: 33,
-    categoryCode: 'F-S-B',
-    categoryName: 'Makanan - Snack - Biskuit',
-    uomSellId: 8,
-    uomSellName: 'DUS',
-    sellPrice: 67000.00,
-    uomBuyId: 8,
-    uomBuyName: 'DUS',
-    buyPrice: 61000.00
-  }, {
-    id: 5,
-    code: 'ABN',
-    name: 'ATM BISKUIT NANAS',
-    description: '',
-    typeId: 6,
-    typeName: 'Resell',
-    qty: 100,
-    uomId: 3,
-    uomCode: 'DUS @ 4 PAK X 6 BKS',
-    categoryId: 33,
-    categoryCode: 'F-S-B',
-    categoryName: 'Makanan - Snack - Biskuit',
-    uomSellId: 8,
-    uomSellName: 'DUS',
-    sellPrice: 64000.00,
-    uomBuyId: 8,
-    uomBuyName: 'DUS',
-    buyPrice: 58000.00
-  }, {
-    id: 6,
-    code: 'ABQ',
-    name: 'ATM BONBIN QUARTO',
-    description: '',
-    typeId: 6,
-    typeName: 'Resell',
-    qty: 100,
-    uomId: 3,
-    uomCode: 'DUS @ 4 PAK X 6 BKS',
-    categoryId: 33,
-    categoryCode: 'F-S-B',
-    categoryName: 'Makanan - Snack - Biskuit',
-    uomSellId: 8,
-    uomSellName: 'DUS',
-    sellPrice: 45000.00,
-    uomBuyId: 8,
-    uomBuyName: 'DUS',
-    buyPrice: 40000.00
-  }, {
-    id: 7,
-    code: 'AC',
-    name: 'ATM CAPUCINO',
-    description: '',
-    typeId: 6,
-    typeName: 'Resell',
-    qty: 100,
-    uomId: 3,
-    uomCode: 'DUS @ 4 PAK X 6 BKS',
-    categoryId: 33,
-    categoryCode: 'F-S-B',
-    categoryName: 'Makanan - Snack - Biskuit',
-    uomSellId: 8,
-    uomSellName: 'DUS',
-    sellPrice: 45000.00,
-    uomBuyId: 8,
-    uomBuyName: 'DUS',
-    buyPrice: 40800.00
-  }]
-}
 
 mock.onPost('/api/item/list').reply((request) => {
   var category = []
   var searchBy = ''
   var search = ''
-
+  
   if (request.data) {
     var { category, searchBy, search } = JSON.parse(request.data)
   }
-
+  
   let results = data.items.filter(i => {
-    if (category.length > 0) return category.includes(i.categoryId)
+    if (category.length > 1) return category.includes(i.categoryId)
     else return i
   })
   
@@ -160,5 +26,14 @@ mock.onPost('/api/item/list').reply((request) => {
     else return i.name.toLowerCase().includes(search.toLowerCase())
   })
   
+  return [200, results]
+})
+
+mock.onGet(/\/api\/item\/\d+/).reply((config) => {
+  const url = config.url.split('/')
+  const id = url[url.length - 1]
+
+  const results = data.items.find(i => i.id == id)
+
   return [200, results]
 })
--- src/fake-db/data/inventory/uom.js
+++ src/fake-db/data/inventory/uom.js
@@ -1,23 +1,5 @@
+import data from '@/fake-db/db.json'
 import mock from '@/fake-db/mock.js'
-
-const data = {
-  uoms: [{
-    id: 1,
-    code: 'DUS @ 6 KLG',
-    name: 'DUS',
-    baseUnit: 'KLG'
-  }, {
-    id: 2,
-    code: 'DUS @ 6 PAK X 20 BKS',
-    name: 'DUS',
-    baseUnit: 'BKS'
-  }, {
-    id: 3,
-    code: 'DUS @ 4 PAK X 6 BKS',
-    name: 'DUS',
-    baseUnit: 'BKS'
-  }]
-}
 
 mock.onPost('/api/uom/list').reply((request) => {
   const datas = data.uoms
