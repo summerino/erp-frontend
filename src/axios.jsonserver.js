94608f776dbdcc743e2a91d89c158d18f6260c8c
Rudy Wang <rudywang87@gmail.com>
2021-02-01 17:58:11 +0700

    adding axios wrapper for json-server

---
+++ src/axios.jsonserver.js
@@ -0,0 +1,10 @@
+import axios from 'axios'
+
+// Set config defaults when creating the instance
+export default axios.create({
+  baseURL: process.env.VUE_APP_JSON_SERVER_BASE_URL || '',
+  headers: {
+    'Accept': 'application/json',
+    'Content-Type': 'application/json' 
+  }
+})
