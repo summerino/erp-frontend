6e38a8fe18668e582c009f2f209f27e830661146
Rudy Wang <rudywang87@gmail.com>
2021-02-01 23:47:39 +0700

    Change api tax using json-server

--- src/fake-db/data/tax/index.js
+++ src/fake-db/data/tax/index.js
@@ -1,10 +1,12 @@
-import data from '@/fake-db/db.json'
 import mock from '@/fake-db/mock.js'
+import axiosJsonServer from '@/axios.jsonserver'
 
-mock.onPost('/api/tax/lists').reply((request) => {
+mock.onPost('/api/tax/lists').reply(async (request) => {
   const { src } = JSON.parse(request.data)
   
-  let results = data.taxes.filter(t => {
+  const response = await axiosJsonServer.get('/taxes')
+  
+  let results = response.data.filter(t => {
     if (src.toLowerCase() == 'sls') return t.showInSales == 1
     else return t
   })
