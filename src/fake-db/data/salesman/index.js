73993405a5b1908e1331fa275e46084838594554
Rudy Wang <rudywang87@gmail.com>
2021-02-02 00:01:13 +0700

    Move salesman data to db.json

--- src/fake-db/data/salesman/index.js
+++ src/fake-db/data/salesman/index.js
@@ -1,45 +1,14 @@
 import mock from '@/fake-db/mock.js'
-import { sortBy as _sortBy  } from 'lodash'
+import axiosJsonServer from '@/axios.jsonserver'
 
-const datas = [{
-  code: 'W00001',
-  name: 'Moon Ga Young'
-}, {
-  code: 'W00002',
-  name: 'Song Hye Kyo'
-}, {
-  code: 'W00003',
-  name: 'Kim Seon Ho'
-}, {
-  code: 'W00004',
-  name: 'Nam Joo Hyuk'
-}, {
-  code: 'W00005',
-  name: 'Bae Suzy'
-}, {
-  code: 'W00006',
-  name: 'Kang Han Na'
-}, {
-  code: 'W00007',
-  name: 'Cha Eun Woo'
-}, {
-  code: 'W00008',
-  name: 'Hwang In Yeop'
-}, {
-  code: 'W00009',
-  name: 'Jo Woo Ri'
-}]
-
-mock.onPost('/api/salesman/lists').reply((request) => {
+mock.onPost('/api/salesman/lists').reply(async (request) => {
   var filter = null
   if (request.data) {
     var { filter } = JSON.parse(request.data)
   }
+  filter = filter || ''
   
-  const results = datas.filter(c => {
-    if (filter) return c.name.toLowerCase().includes(filter.toLowerCase())
-    else return c
-  })
+  const response = await axiosJsonServer.get(`/salesmans?name_like=${filter}&_sort=name`)
   
-  return [200, _sortBy(results, ['name'])]
+  return [response.status, response.data]
 })
