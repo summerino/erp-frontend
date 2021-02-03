614834623edf0dcf1718d9b16addef2e9a585ebf
Rudy Wang <rudywang87@gmail.com>
2021-02-03 14:51:40 +0700

    Separate mock api item to different file

--- src/fake-db/data/inventory/index.js
+++ src/fake-db/data/inventory/index.js
@@ -1,40 +1,5 @@
-import mock from '@/fake-db/mock.js'
-import axios from 'axios'
-
 import itemCategory from './item.category'
 import itemGroup from './item.group'
-import uom from './uom.conversion'
-
-mock.onPost('/api/item/list').reply(async (request) => {
-  var category = []
-  var searchBy = ''
-  var search = ''
-  
-  if (request.data) {
-    var { category, searchBy, search } = JSON.parse(request.data)
-  }
-
-  const response = await axiosJsonServer.get('/items')
-  
-  let results = response.data.filter(i => {
-    if (category.length > 1) return category.includes(i.categoryId)
-    else return i
-  })
-  
-  results = results.filter(i => {
-    if (searchBy.toLowerCase() == 'code') return i.code.toLowerCase().includes(search.toLowerCase())
-    else if (searchBy.toLowerCase() == 'type') return i.typeName.toLowerCase().includes(search.toLowerCase())
-    else return i.name.toLowerCase().includes(search.toLowerCase())
-  })
-  
-  return [200, results]
-})
-
-mock.onGet(/\/api\/item\/\d+/).reply(async (config) => {
-  const url = config.url.split('/')
-  const id = url[url.length - 1]
-
-  const response = await axiosJsonServer.get(`/items/${id}`)
-
-  return [response.status, response.data]
-})
+import item from './item'
+import uomConversion from './uom.conversion'
+import uom from './uom'
---
+++ src/fake-db/data/inventory/item.js
@@ -0,0 +1,36 @@
+import mock from '@/fake-db/mock.js'
+import axiosJsonServer from '@/axios.jsonserver'
+
+mock.onPost('/api/item/list').reply(async (request) => {
+  var category = []
+  var searchBy = ''
+  var search = ''
+  
+  if (request.data) {
+    var { category, searchBy, search } = JSON.parse(request.data)
+  }
+
+  const response = await axiosJsonServer.get('/items')
+  
+  let results = response.data.filter(i => {
+    if (category.length > 1) return category.includes(i.categoryId)
+    else return i
+  })
+  
+  results = results.filter(i => {
+    if (searchBy.toLowerCase() == 'code') return i.code.toLowerCase().includes(search.toLowerCase())
+    else if (searchBy.toLowerCase() == 'type') return i.typeName.toLowerCase().includes(search.toLowerCase())
+    else return i.name.toLowerCase().includes(search.toLowerCase())
+  })
+  
+  return [200, results]
+})
+
+mock.onGet(/\/api\/item\/\d+/).reply(async (config) => {
+  const url = config.url.split('/')
+  const id = url[url.length - 1]
+
+  const response = await axiosJsonServer.get(`/items/${id}`)
+
+  return [response.status, response.data]
+})
