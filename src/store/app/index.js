ef2c72c0f2f1e39a3f7249f4cece4d3bcfa6d932
Galang Bilal Farchan <galang.bilal@sahassa.co.id>
2021-04-20 09:35:51 +0700

    Add master customer type & validation on customer

--- src/store/app/index.js
+++ src/store/app/index.js
@@ -39,11 +39,20 @@ const state = {
     above0: [
       (v) => parseFloat((v || '0').replace(/,/g, '')) > 0 || 'Value should above 0'
     ],
+    max15chars: [
+      (v) => (v || '').length <= 15 || 'Max 15 characters'
+    ],
     max20chars: [
       (v) => (v || '').length <= 20 || 'Max 20 characters'
     ],
     max30chars: [
       (v) => (v || '').length <= 30 || 'Max 30 characters'
+    ],
+    max50chars: [
+      (v) => (v || '').length <= 50 || 'Max 50 characters'
+    ],
+    max100chars: [
+      (v) => (v || '').length <= 100 || 'Max 100 characters'
     ],
     max256chars: [
       (v) => (v || '').length <= 256 || 'Max 256 characters'
