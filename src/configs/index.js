cd507b95edfc0cef2ea7897e8369aa5b351d03d5
Wildan Nugraha <wildan.nugraha@sahassa.co.id>
2021-05-18 19:14:41 +0700

    refactor advanced search

--- src/configs/index.js
+++ src/configs/index.js
@@ -8,6 +8,7 @@ import analytics from './analytics'
 import navigation from './navigation'
 import currency from './currency'
 import endpoint from './endpoint'
+import filter from './filter'
 
 export default {
   // product display information
@@ -44,5 +45,8 @@ export default {
   navigation,
 
   // endpoint configs
-  endpoint
+  endpoint,
+  
+  // filter config
+  filter
 }
--- src/store/app/index.js
+++ src/store/app/index.js
@@ -2,7 +2,7 @@ import configs from '../../configs'
 import mutations from './mutations'
 import actions from './actions'
 
-const { product, time, theme, currency } = configs
+const { product, time, theme, currency, filter } = configs
 
 const { globalTheme, menuTheme, toolbarTheme, isToolbarDetached, isContentBoxed, isRTL } = theme
 
@@ -79,67 +79,8 @@ const state = {
   loadOverlay: false,
 
   // App.vue advanced search filter
-  isAdvancedSearch: false,
-  filter: {
-    operator: [
-      { text: 'Equal', value: 'eq', allowedCategoryTypes: ['number', 'datetime', 'text', 'bit'] },
-      { text: 'Not Equal', value: 'neq', allowedCategoryTypes: ['number', 'datetime', 'text', 'bit'] },
-      { text: 'Start With', value: 'startwith', allowedCategoryTypes: ['text'] },
-      { text: 'End With', value: 'endswith', allowedCategoryTypes: ['text'] },
-      { text: 'Contains', value: 'contains', allowedCategoryTypes: ['text'] },
-      { text: 'Not Contains', value: 'doesnotcontain', allowedCategoryTypes: ['text'] },
-      { text: 'Less than', value: 'lt', allowedCategoryTypes: ['number', 'datetime'] },
-      { text: 'Greater than', value: 'gt', allowedCategoryTypes: ['number', 'datetime'] },
-      { text: 'Less than equal', value: 'lte', allowedCategoryTypes: ['number', 'datetime'] },
-      { text: 'Greater than equal', value: 'gte', allowedCategoryTypes: ['number', 'datetime'] }
-    ],
-    mapDataTypeToCategory: [
-      {
-        category: 'number',
-        dataTypes: [
-          'bigint',
-          'decimal',
-          'float',
-          'int',
-          'money',
-          'real',
-          'smallint',
-          'smallmoney',
-          'tinyint'
-        ]
-      },
-      {
-        category: 'text',
-        dataTypes: [
-          'nchar',
-          'ntext',
-          'nvarchar',
-          'text',
-          'varchar',
-          'char'
-        ]
-      },
-      {
-        category: 'datetime',
-        dataTypes: [
-          'datetime',
-          'timestamp',
-          'date',
-          'time',
-          'datetime2'
-        ]
-      },
-      {
-        category: 'bit',
-        dataTypes: [
-          'bit'
-        ]
-      }
-    ],
-    fields: [],
-    search: '',
-    searches: []
-  }
+  filter
+  
 }
 
 export default {
