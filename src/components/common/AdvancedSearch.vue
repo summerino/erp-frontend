09b4c7d215963449ab382e0cdd19f454a5744a15
Wildan Nugraha <wildan.nugraha@sahassa.co.id>
2021-05-20 17:52:49 +0700

    change location button cari from right to left

--- src/components/common/AdvancedSearch.vue
+++ src/components/common/AdvancedSearch.vue
@@ -125,17 +125,17 @@
           <v-row class="pl-2 mt-0 pt-0" no-gutters>
             <v-col cols="10 text-right">
               <v-btn
-                small
-                text
-                @click="resetAdvancedFilter()"
-              >Batal</v-btn>
-              <v-btn
                 class="primary"
                 text
                 small
                 @click="advancedSearch()"
                 tile
               >Cari</v-btn>
+              <v-btn
+                small
+                text
+                @click="resetAdvancedFilter()"
+              >Batal</v-btn>
             </v-col>
           </v-row>
         </div>
@@ -168,7 +168,7 @@ export default {
     addSearch() {
       this.$store.commit('app/addSearch')
     },
-    advancedSearch() {      
+    advancedSearch() {
       const filters = this.filter.searches.filter(x => x.operator !== '' && x.field !== '' && x.keyword !== '')
       this.getList(false, filters)
     },
@@ -201,7 +201,7 @@ export default {
         bindToForm: bindToForm,
         filters: filters,
         search: this.search,
-        isAdvancedSearch: filter.isAdvancedSearch
+        isAdvancedSearch: this.filter.isAdvancedSearch
       }
       this.$emit('search', vm)
     }
