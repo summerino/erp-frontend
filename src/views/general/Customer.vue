14a771cfa0ec7029e385dad4c7ce5531c938bff3
Galang Bilal Farchan <galang.bilal@sahassa.co.id>
2021-06-23 17:11:41 +0700

    Change Credit Term to Payment Term

--- src/views/general/Customer.vue
+++ src/views/general/Customer.vue
@@ -249,13 +249,16 @@
 
               <v-row no-gutters>
                 <v-col cols="12" md="6" class="pr-md-3">
-                  <v-currency-field
-                    v-model="data.creditTerm"
-                    :decimal-length="0"
-                    class="mt-0"
-                    label="Jangka Waktu Kredit"
-                    :max="32767"
-                  ></v-currency-field>
+                  <v-autocomplete
+                  v-model="data.paymentTermId"
+                  :items="paymentTerms"
+                  :item-text="item => `${item.initial} - ${item.name}`"
+                  :rules="rules.required"
+                  label="Pembayaran"
+                  item-value="id"
+                  class="mt-0"
+                  >
+                  </v-autocomplete>
                 </v-col>
                 <v-col cols="12" md="6" class="pl-md-3">
                   <v-currency-field
@@ -611,6 +614,7 @@ export default {
     billingAddressRef: [],
     shippingAddressRef: [],
     areaRef: [],
+    paymentTerms: [],
     data: {}
   }),
 
@@ -618,6 +622,7 @@ export default {
     this.getList()
     this.getTypesList()
     this.getAreaList()
+    this.getPaymentTermLists()
     auth.getAction(this.endpoint, this.menuId.customer, [this.action.insert, this.action.update, this.action.delete])
       .then((response) => {
         this.$store.commit('api/setAuth', response.data)
@@ -748,6 +753,12 @@ export default {
       api.getAll(`${this.endpoint.sales.area}/lists`, {})
         .then(response => {
           this.areaRef = response.data.tableData
+        })
+    },
+    getPaymentTermLists() {
+      api.getAll('payment-term/lists')
+        .then(response => {
+          this.paymentTerms = response.data.tableData
         })
     },
     back() {
--- src/views/sales/transaction/SalesInvoice.vue
+++ src/views/sales/transaction/SalesInvoice.vue
@@ -742,7 +742,7 @@
 
 <script>
 import { mapState } from 'vuex'
-import { format, parseISO } from 'date-fns'
+import { format, parseISO, addDays } from 'date-fns'
 import { sumBy as _sumBy } from 'lodash'
 
 import { randomNumber } from '@/helpers/math-helpers'
@@ -847,13 +847,15 @@ export default {
     ], 
     valid: false,
     employees: [],
-    dlvOrders:[],
+    dlvOrders: [],
+    paymentTerms: [],
     data: {}
   }),
 
   created: function () {
     this.getList()
     this.getEmployeeLists()
+    this.getPaymentTermLists()
     auth.getAction(this.endpoint, this.menuId.salesinvoice, [this.action.insert, this.action.update, this.action.void])
       .then((response) => {
         this.$store.commit('api/setAuth', response.data)
@@ -1109,6 +1111,12 @@ export default {
         this.$refs.code.focus()
       }, 0)
     },
+    getPaymentTermLists() {
+      api.getAll('payment-term/lists')
+        .then(response => {
+          this.paymentTerms = response.data.tableData
+        })
+    },
     async remove(item) {
       if (
         await this.$refs.confirm.open(
@@ -1271,6 +1279,13 @@ export default {
             item.custAddr = response.data.address1
             item.custPhone = response.data.phone
             item.custFax = response.data.fax
+
+            const paymentData = this.paymentTerms.find(x => x.id === response.data.paymentTermId)
+            if (paymentData) {
+              const date = addDays(parseISO(this.data.date), paymentData.due)
+              this.data.dueDate = format(date, 'yyyy-MM-dd')
+            }
+
           }
         })
     },
