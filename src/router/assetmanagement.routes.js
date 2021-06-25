ab5f4a086a72b573828c2b5a9c4251b9fe1c48e9
Wildan Nugraha <wildan.nugraha@sahassa.co.id>
2021-06-25 09:03:37 +0700

    rename routes

--- src/router/assetmanagement.routes.js
+++ src/router/assetmanagement.routes.js
@@ -1,14 +1,16 @@
 export default [
   {
+    path: '/asset-management/fixed-asset',
+    name: 'fixed-asset',
+    component: () => import(/* webpackChunkName: "fixed-asset" */ '@/views/assetmanagement/Asset.vue'),
+    meta: {
+      authRequired: true
+    }
+  },
+  {
     path: '/asset-management/asset-type',
     name: 'asset-type',
     component: () => import(/* webpackChunkName: "asset-type" */ '@/views/assetmanagement/AssetType.vue'),
-    meta: {
-      authRequired: true
-    },
-    path: '/asset-management/asset',
-    name: 'asset',
-    component: () => import(/* webpackChunkName: "asset" */ '@/views/assetmanagement/Asset.vue'),
     meta: {
       authRequired: true
     }
