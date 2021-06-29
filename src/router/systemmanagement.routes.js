9d013df8ffc870c41e111b475d91a0df32afdf39
Rudy Wang <rudywang87@gmail.com>
2021-06-29 16:30:16 +0700

    Rearrange routes
    Related Work Item: #3343

--- src/router/systemmanagement.routes.js
+++ src/router/systemmanagement.routes.js
@@ -1,14 +1,14 @@
 export default [{
-  path: '/systemmanagement/user',
+  path: '/system-management/user',
   name: 'user',
   component: () => import(/* webpackChunkName: "systemmanagement-user" */ '@/views/systemmanagement/User.vue'),
   meta: {
     authRequired: true
   }
 }, {
-  path: '/systemmanagement/role',
-  name: 'role',
-  component: () => import(/* webpackChunkName: "systemmanagement-role" */ '@/views/systemmanagement/Role.vue'),
+  path: '/system-management/user-role',
+  name: 'user-role',
+  component: () => import(/* webpackChunkName: "systemmanagement-user-role" */ '@/views/systemmanagement/Role.vue'),
   meta: {
     authRequired: true
   }
