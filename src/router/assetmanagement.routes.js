a8a53ea9c5024292fa4f9abb1a07735c2529d53b
Rudy Wang <rudywang87@gmail.com>
2021-06-29 15:24:18 +0700

    Rearrange project folder
    Related Work Item: #3343

--- src/router/assetmanagement.routes.js
+++ src/router/assetmanagement.routes.js
@@ -1,16 +1,15 @@
 export default [
   {
-    path: '/asset-management/fixed-asset',
-    name: 'fixed-asset',
-    component: () => import(/* webpackChunkName: "fixed-asset" */ '@/views/assetmanagement/FixedAsset.vue'),
+    path: '/asset-management/master/asset-type',
+    name: 'asset-type',
+    component: () => import(/* webpackChunkName: "asset-type" */ '@/views/assetmanagement/master/AssetType.vue'),
     meta: {
       authRequired: true
     }
-  },
-  {
-    path: '/asset-management/asset-type',
-    name: 'asset-type',
-    component: () => import(/* webpackChunkName: "asset-type" */ '@/views/assetmanagement/AssetType.vue'),
+  }, {
+    path: '/asset-management/transaction/fixed-asset',
+    name: 'fixed-asset',
+    component: () => import(/* webpackChunkName: "fixed-asset" */ '@/views/assetmanagement/transaction/FixedAsset.vue'),
     meta: {
       authRequired: true
     }
