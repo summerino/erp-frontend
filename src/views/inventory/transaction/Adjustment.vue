19c53de985f6e47a215306cab19865dde6de3e34
Wildan Nugraha <wildan.nugraha@sahassa.co.id>
2021-07-01 17:50:53 +0700

    change icon, change language, add type in grid adjustment

--- src/views/inventory/transaction/Adjustment.vue
+++ src/views/inventory/transaction/Adjustment.vue
@@ -665,6 +665,7 @@ export default {
         { value: 'action', sortable: false, divider: true, width: '90', excelColWidth:'10' },
         { text: 'Tanggal', value: 'date', divider: true, width: '150', excelColWidth:'18', isDateTime: true },
         { text: 'Kode', value: 'code', divider: true, width: '150', excelColWidth:'18' },
+        { text: 'Tipe', value: 'types', divider: true, width: '150', excelColWidth:'18' },
         { text: 'Lokasi', value: 'warehouseInitial', divider: true, width: '150', excelColWidth:'18' },
         { text: 'Catatan', value: 'notes', divider: true, width: '200', excelColWidth:'25' },
         { text: 'Status', value: 'mark', divider: true, width: '200', excelColWidth:'20' }
@@ -687,6 +688,21 @@ export default {
       },
       {
         text: 'Kode', value: 'code', dataType: 'text'
+      },
+      {
+        text: 'Tipe', 
+        value: 'type', 
+        dataType: 'bit',
+        options: [
+          { 
+            text: 'Penyesuaian',
+            value: '1'
+          },
+          { 
+            text: 'Perhitungan Persediaan',
+            value: '2'
+          }
+        ]
       },
       {
         text: 'Lokasi', value: 'warehouseInitial', dataType: 'text'
