24a0b7f62bc229d736a211d8938ed273f868510e
Galang Bilal Farchan <galang.bilal@sahassa.co.id>
2021-07-01 14:34:23 +0700

    Adding Beginning Balance AR&AP Frontend & fix routes

--- src/store/app/index.js
+++ src/store/app/index.js
@@ -50,6 +50,9 @@ const state = {
     max15chars: [
       (v) => (v || '').length <= 15 || 'Maksimal 15 karakter'
     ],
+    max17chars: [
+      (v) => (v || '').length <= 17 || 'Maksimal 17 karakter'
+    ],
     max20chars: [
       (v) => (v || '').length <= 20 || 'Maksimal 20 karakter'
     ],
---
+++ src/views/accounting/transaction/begin-balance/AccountReceiveable.vue
@@ -0,0 +1,568 @@
+<template>
+  <div class="w-full">
+    <v-card v-if="main">
+      <v-card-title class="indigo--text text--lighten-2 pb-1">
+        <v-row dense>
+          <v-col cols="12" md="2">
+            Piutang
+          </v-col>
+          <v-col cols="12" md="4">
+            <v-text-field
+              v-model="grid.search"
+              append-icon="mdi-magnify"
+              label="Cari..."
+              class="font-weight-regular mt-0 pt-0"
+              single-line
+              @keyup.enter="getList"
+            ></v-text-field>
+          </v-col>
+          <v-spacer></v-spacer>
+          <v-col cols="12" md="1">
+            <export-excel title="Daftar Piutang" :grid="grid" :gridDefOpts="gridDefOpts" ref="exportExcel"></export-excel>
+          </v-col>
+          <v-col cols="12" md="5" class="text-right">
+            <v-tooltip bottom>
+              <template v-slot:activator="{ on, attrs }">
+                <v-btn
+                  v-bind="attrs"
+                  v-on="on"
+                  v-shortkey="['ctrl', 'alt', 'n']"
+                  color="green darken-1"
+                  class="font-weight-regular"
+                  dark
+                  small
+                  tile
+                  @click="add"
+                  @shortkey="add"
+                  :disabled="!auth.allowInsert"
+                >
+                  <v-icon left>mdi-plus</v-icon>
+                  Data Baru
+                </v-btn>
+              </template>
+              <span class="text-caption">(Ctrl + Alt + N)</span>
+            </v-tooltip>
+          </v-col>
+        </v-row>
+      </v-card-title>
+
+      <v-data-table
+        :headers="grid.columns"
+        :footer-props="{ itemsPerPageOptions: gridDefOpts.pageSizes }"
+        :height="gridDefOpts.height"
+        :items="grid.data"
+        :items-per-page="gridDefOpts.pageSize"
+        :options.sync="grid.options"
+        :sort-by="grid.options.sortBy"
+        :sort-desc="grid.options.sortDesc"
+        class="elevation-1"
+        fixed-header
+      >
+        <template v-slot:[`item.action`]="{ item }">
+          <v-tooltip bottom>
+            <template v-slot:activator="{ on, attrs }">
+              <v-btn
+                v-bind="attrs"
+                v-on="on"
+                icon
+                small
+                color="orange lighten-1"
+                @click="edit(item)"
+              >
+                <v-icon small>mdi-pencil</v-icon>
+              </v-btn>
+            </template>
+            <span>Ubah</span>
+          </v-tooltip>
+          <v-tooltip bottom>
+            <template v-slot:activator="{ on, attrs }">
+              <v-btn
+                v-bind="attrs"
+                v-on="on"
+                icon
+                small
+                color="red"
+                @click="remove(item)"
+                :disabled="!auth.allowDelete"
+              >
+                <v-icon small>mdi-close-thick</v-icon>
+              </v-btn>
+            </template>
+            <span>Hapus</span>
+          </v-tooltip>
+        </template>
+        <template v-slot:[`item.date`]="{ item }">
+          {{ item.date | formatDate('dd-MMM-yyyy') }}
+        </template>
+        <template v-slot:[`item.dueDate`]="{ item }">
+          {{ item.dueDate | formatDate('dd-MMM-yyyy') }}
+        </template>
+        <template v-slot:[`item.amount`]="{ item }">
+          {{ item.amount | formatCurrency }}
+        </template>
+      </v-data-table>
+    </v-card>
+
+    <v-card v-else>
+      <v-card-title class="indigo--text text--lighten-2 pb-1">
+        <v-row dense>
+          <v-col cols="12" md="4">
+            <span>{{ data.action === 'add' ? 'Tambah' : 'Ubah' }} Piutang</span>
+          </v-col>
+          <v-col cols="12" md="8" class="text-right">
+            <label
+              v-if="data.action == 'edit'"
+              class="text-caption mr-1"
+            >
+              Tanggal Diperbarui: {{ data.updatedDate }} oleh {{ data.updatedInitial }}
+            </label>
+            <v-tooltip bottom>
+              <template v-slot:activator="{ on, attrs }">
+                <v-btn
+                  v-bind="attrs"
+                  v-on="on"
+                  v-shortkey="['ctrl', 'enter']"
+                  color="blue darken-2"
+                  class="font-weight-regular"
+                  :disabled="isActive || (data.action === 'edit' && !auth.allowUpdate)"
+                  dark
+                  small
+                  tile
+                  @click="save"
+                  @shortkey="save"
+                  
+                >
+                  <v-icon left>
+                    mdi-content-save
+                  </v-icon>
+                  Simpan
+                </v-btn>
+              </template>
+              <span class="text-caption">(Ctrl + Enter)</span>
+            </v-tooltip>
+            <v-tooltip bottom>
+              <template v-slot:activator="{ on, attrs }">
+                <v-btn
+                  v-bind="attrs"
+                  v-on="on"
+                  v-shortkey="['esc']"
+                  class="font-weight-regular ml-1"
+                  small
+                  tile
+                  @click="back"
+                  @shortkey="back"
+                >
+                  <v-icon left>
+                    mdi-undo-variant
+                  </v-icon>
+                  Kembali
+                </v-btn>
+              </template>
+              <span class="text-caption">(Esc)</span>
+            </v-tooltip>
+          </v-col>
+        </v-row>
+      </v-card-title>
+
+      <v-card-text>
+        <v-form
+          ref="form"
+          v-model="valid"
+        >
+          <v-container fluid grid-list-md>
+            <v-row no-gutters>
+              <v-col cols="12" md="6" class="pr-md-3">
+                <v-text-field
+                  ref="code"
+                  v-model="data.code"
+                  :rules="[rules.required[0], rules.max17chars[0]]"
+                  :counter="17"
+                  label="Kode"
+                  class="mt-0"
+                  required
+                ></v-text-field>
+              </v-col>
+              <v-col cols="12" md="6" class="pl-md-3">
+                <v-autocomplete
+                  v-model="data.custCode"
+                  :items="customers"
+                  :item-text="item => `${item.code} - ${item.name}`"
+                  :rules="rules.required"
+                  label="Pelanggan"
+                  item-value="code"
+                  class="mt-0"
+                  required
+                ></v-autocomplete>
+              </v-col>
+            </v-row>
+
+            <v-row no-gutters>
+              <v-col cols="12" md="6" class="pr-md-3">
+                <v-menu
+                  v-model="menu.date"
+                  :close-on-content-click="false"
+                  transition="scale-transition"
+                  min-width="290px"
+                  offset-y
+                >
+                  <template v-slot:activator="{ on, attrs }">
+                    <v-text-field
+                      v-bind="attrs"
+                      v-on="on"
+                      :rules="rules.required"
+                      :value="formatDate"
+                      label="Tanggal"
+                      class="mt-0"
+                      readonly
+                      required
+                    ></v-text-field>
+                  </template>
+                  <v-date-picker
+                    v-model="data.date"
+                    no-title
+                    scrollable
+                    @change="menu.date = false"
+                  ></v-date-picker>
+                </v-menu>
+              </v-col>
+              <v-col cols="12" md="6" class="pl-md-3">
+                <v-menu
+                  v-model="menu.dueDate"
+                  :close-on-content-click="false"
+                  transition="scale-transition"
+                  min-width="290px"
+                  offset-y
+                >
+                  <template v-slot:activator="{ on, attrs }">
+                    <v-text-field
+                      v-bind="attrs"
+                      v-on="on"                      
+                      :rules="rules.required"
+                      :value="formatDueDate"
+                      label="Tanggal Jatuh Tempo"
+                      class="mt-0"
+                      readonly
+                      required
+                    ></v-text-field>
+                  </template>
+                  <v-date-picker
+                    v-model="data.dueDate"
+                    no-title
+                    scrollable
+                    @change="menu.dueDate = false"
+                  ></v-date-picker>
+                </v-menu>
+              </v-col>
+            </v-row>
+
+            <v-row no-gutters>
+              <v-col cols="12" md="6" class="pr-md-3">
+                <v-autocomplete
+                  v-model="data.currCode"
+                  :items="currencies"
+                  :item-text="item => `${item.code} - ${item.name}`"
+                  :rules="rules.required"
+                  label="Kurensi"
+                  item-value="code"
+                  class="mt-0"
+                  required
+                ></v-autocomplete>
+              </v-col>
+              <v-col cols="12" md="6" class="pl-md-3">
+                <v-currency-field
+                  v-model="data.currRate"
+                  :decimal-length="0"
+                  class="mt-0"
+                  label="Nilai tukar mata uang"
+                ></v-currency-field>
+              </v-col>
+            </v-row>
+
+            <v-row no-gutters>
+              <v-col cols="12" md="6" class="pr-md-3">
+                <v-currency-field
+                  v-model="data.amount"
+                  :decimal-length="0"
+                  class="mt-0"
+                  label="Nilai"
+                ></v-currency-field>
+              </v-col>
+              <v-col cols="12" md="6" class="pl-md-3">
+                  <v-text-field
+                    v-model="data.notes"
+                    :rules="rules.max256chars"
+                    :counter="256"
+                    class="mt-0"
+                    label="Catatan"
+                  ></v-text-field>
+                </v-col>
+            </v-row>
+          </v-container>
+        </v-form>
+      </v-card-text>
+    </v-card>
+    
+    <confirm ref="confirm"></confirm>
+  </div>
+</template>
+
+<script>
+import { mapState } from 'vuex'
+import { format, parseISO }  from 'date-fns'
+
+import api from '@/services/axios.service'
+import auth from '@/services/authorization.service'
+
+import ExportExcel from '@/components/common/ExportExcel.vue'
+import Confirm from '@/components/dialog/Confirm'
+
+export default {
+  components:{
+    ExportExcel,
+    Confirm
+  },
+
+  data: () => ({
+    main: true,
+    menu: {
+      date: false,
+      dueDate: false
+    },
+    grid: {
+      columns: [
+        { value: 'action', sortable: false, divider: true, width: '90', excelColWidth:'10' },
+        { text: 'Kode', value: 'code', divider: true, width: '150', excelColWidth:'20' },
+        { text: 'Pelanggan', value: 'custName', divider: true, width: '150', excelColWidth:'20' },
+        { text: 'Tanggal', value: 'date', align: 'right', divider: true, width: '100', excelColWidth:'20'  },
+        { text: 'Tanggal Jatuh Tempo', value: 'dueDate', align: 'right', divider: true, width: '100', excelColWidth:'20' },
+        { text: 'Mata Uang', value: 'currCode', divider: true, width: '120', excelColWidth:'20' },
+        { text: 'Nilai', value: 'amount', align: 'right', divider: true, width: '100', excelColWidth:'20' }
+      ],
+      data: [],
+      options: {
+        sortBy: ['code'],
+        sortDesc: [false]
+      },
+      total: 0,
+      search: null
+    },
+    valid: false,
+    currencies: [],
+    customers: [],
+    types: [],
+    data: {}
+  }),
+
+  created: function () {
+    this.getList()
+    this.getCurrencyLists()
+    this.getCustomerLists()
+    auth.getAction(this.endpoint, this.menuId.accountReceivable, [this.action.insert, this.action.update, this.action.delete])
+      .then((response) => {
+        this.$store.commit('api/setAuth', response.data)
+      })
+  },
+
+  mounted: function () {
+    setTimeout(() => {
+      this.$store.commit('app/setBreadcrumbs', [{
+        text: 'Akuntansi'
+      }, {
+        text: 'Tranksasi'
+      }, {
+        text: 'Saldo Awal'
+      }, {
+        text: 'Piutang'
+      }])
+      this.$store.commit('app/setGridDefaultHeight', this.$el.clientHeight)
+    }, 0)
+  },
+
+  watch: {
+    'grid.options': {
+      handler() {
+        this.getList()
+      },
+      deep: true
+    }
+  },
+
+  computed: {
+    ...mapState({
+      gridDefOpts: state => state.app.grid,
+      rules: state => state.app.rules,
+      endpoint: state => state.api.endpoint,
+      auth: state => state.api.authorization,
+      action: state => state.api.action,
+      menuId: state => state.api.menus
+    }),
+    isActive() {
+      return (!this.data.isActive)
+    },
+    formatDate() {
+      return this.data.date ? format(parseISO(this.data.date), 'dd-MMM-yyyy') : ''
+    },
+    formatDueDate() {
+      return this.data.dueDate ? format(parseISO(this.data.dueDate), 'dd-MMM-yyyy') : ''
+    }
+  },
+  
+  methods:{
+    reset(resetValidation = true) {
+      this.data = {
+        action: '',
+        initial: null,
+        name: null,
+        isActive: true
+      }
+
+      // Reset form validation
+      if (resetValidation) {
+        setTimeout(() => {
+          this.$refs.form.resetValidation()
+        }, 0)
+      }
+    },
+    getList(bindToForm = false) {
+      const sorts = []
+      for (let i = 0; i < this.grid.options.sortBy.length; i++) {
+        sorts.push({
+          field: this.grid.options.sortBy[i],
+          direction: this.grid.options.sortDesc[i] ? 'desc' : 'asc'
+        })
+      }
+      
+      api.getAll(this.endpoint.accounting.accountReceivable, {
+        params: {
+          search: this.grid.search,
+          skip: ((this.grid.options.page - 1) * this.grid.options.itemsPerPage) || 0,
+          take: this.grid.options.itemsPerPage || this.gridDefOpts.pageSize,
+          sorts: JSON.stringify(sorts),
+          filters: JSON.stringify([
+            {
+              field: 'isActive',
+              operator: 'eq',
+              keyword: 'true'
+            }
+          ])
+        }
+      })
+        .then(response => {
+          this.grid.data = response.data.tableData
+          this.grid.total = response.data.rowCount
+          if (bindToForm) {
+            const item = this.grid.data.find(h => h.id === this.data.id)
+            this.edit(item)
+          }
+        })
+    },
+    back() {
+      this.main = true
+    },
+    add() {
+      this.main = false
+      this.reset(false)
+      this.data.action = 'add'
+
+      setTimeout(() => {
+        // Set focus to code field
+        this.$refs.code.focus()
+
+        // Validate form first
+        this.$refs.form.validate()
+      }, 0)
+    },
+    edit(item) {
+      if (!item) return
+
+      this.main = false
+      this.reset()
+
+      this.data = {
+        ...item,
+        action: 'edit',
+        updatedDate: format(parseISO(item.updatedDate), 'dd-MMM-yyyy HH:mm:ss')
+      }
+    },
+    async remove(item) {
+      if (
+        await this.$refs.confirm.open(
+          'Hapus Data?',
+          'Apakah anda yakin untuk menghapus data ini?')
+      ) {
+        api.delete(this.endpoint.accounting.accountReceivable, item.id)
+          .then(response => {
+            if (response.data.success) {
+              this.$store.dispatch('app/showSuccess', response.data.message)
+              this.getList()
+            }
+          })
+      }
+    },
+    async save() {
+      if (!this.$refs.form.validate()) {
+        this.$store.dispatch('app/showInfo', 'Mohon periksa kembali inputan yang wajib diisi atau yang terdapat kesalahan.')
+        return
+      }
+
+      let result = { success: false, message: '' }
+      if (this.data.action === 'add') {
+        const resp = await api.create(this.endpoint.accounting.accountReceivable, this.data)
+        result = resp.data
+      } else if (this.data.action === 'edit') {
+        const resp = await api.update(this.endpoint.accounting.accountReceivable, this.data.id, this.data)
+        result = resp.data
+      }
+
+      if (result.success) {
+        this.$store.dispatch('app/showSuccess', result.message)
+        this.back()
+        this.getList()
+      }
+    },
+    async exportExcel() {
+      this.exportExcel.export()
+    },
+    getCurrencyLists() {
+      api.getAll(`${this.endpoint.general.currency}/lists`, {
+        params: {
+          filters: JSON.stringify([{
+            field: 'isActive',
+            operator: 'eq',
+            keyword: true
+          }]),
+          sorts: JSON.stringify([{
+            field: 'code',
+            direction: 'asc'
+          }])
+        }
+      })
+        .then(response => {
+          this.currencies = response.data.tableData
+        })
+    },
+    getCustomerLists() {
+      api.getAll(`${this.endpoint.general.customer.customer}/lists`, {
+        params: {
+          filters: JSON.stringify([{
+            field: 'isActive',
+            operator: 'eq',
+            keyword: true
+          }]),
+          sorts: JSON.stringify([{
+            field: 'initial',
+            direction: 'asc'
+          }])
+        }
+      })
+        .then(response => {
+          this.customers = response.data.tableData
+        })
+    }
+  }
+}
+
+</script>
+
+<style>
+</style>
