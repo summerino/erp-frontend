7c52a8bfa099e504715a95ea93bd8c870e3702c7
Galang Bilal Farchan <galang.bilal@sahassa.co.id>
2021-07-07 10:02:23 +0700

    Change void message to bahasa indonesia

--- src/views/sales/master/Promo.vue
+++ src/views/sales/master/Promo.vue
@@ -885,7 +885,7 @@ export default {
       if (
         await this.$refs.confirm.open(
           'Void?',
-          'Are you sure want to void this data?')
+          'Apakah anda yakin ingin membuat void data ini?')
       ) {
         api.delete(this.endpoint.sales.promo, item.code)
           .then(response => {
