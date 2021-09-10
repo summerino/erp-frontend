b7823e33624fa985002b196fc56e6cce2541f763
Galang Bilal Farchan <galang.bilal@sahassa.co.id>
2021-09-10 14:45:42 +0700

    enable button edit

--- src/views/purchase/transaction/PurchaseOrder.vue
+++ src/views/purchase/transaction/PurchaseOrder.vue
@@ -87,7 +87,6 @@
               <v-btn
                 v-bind="attrs"
                 v-on="on"
-                :disabled="item.mark.toUpperCase() === 'CLS'"
                 color="orange lighten-1"
                 icon
                 small
