96ad5a357589d6403f0c7be8d11ab5c16fe5f4d7
Rudy Wang <rudy.sepbroto@sahassa.co.id>
2021-10-26 09:23:33 +0700

    remove console

--- src/views/sales/transaction/DirectInvoice.vue
+++ src/views/sales/transaction/DirectInvoice.vue
@@ -2129,8 +2129,6 @@ export default {
             gridData[k].discPromo = discPromo
             gridData[k].disc = totalDisc
             this.calcItemPrice(gridData[k], false)
-            // gridData[k].nettPrice = gridData[k].unitPrice - totalDisc 
-            // gridData[k].total =  gridData[k].nettPrice * gridData[k].qty
           } else if (discPromo.length > 0 && gridData[k].discPromo.length > 0) {
             const nDiscPromo = []
             for (let ip = 0; ip < discPromo.length; ip++) {
@@ -2143,13 +2141,10 @@ export default {
                 nDiscPromo.push(gridData[k].discPromo[iq])
               }
             }
-            console.log(nDiscPromo)
             if (nDiscPromo.length > 0) {
               gridData[k].discPromo = nDiscPromo
               gridData[k].disc = _sumBy(gridData[k].discPromo, 'amount') 
               this.calcItemPrice(gridData[k], false)
-              // gridData[k].nettPrice += gridData[k].unitPrice - _sumBy(discPromo, 'totalDisc') 
-              // gridData[k].total +=  gridData[k].nettPrice * gridData[k].qty
             }
           }
         } else {
