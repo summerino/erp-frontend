9ba9bb78eaaf21a51265c08c0ed732cb82e50ee5
Galang Bilal Farchan <galang.bilal@sahassa.co.id>
2022-01-11 10:43:06 +0700

    Fix dpp value & duplicate column in purchase report

--- src/views/purchase/report/PurchaseReceiveReport.vue
+++ src/views/purchase/report/PurchaseReceiveReport.vue
@@ -546,7 +546,6 @@ export default {
       { text: 'Nm. Barang', value: 'itemName', divider: true, width: '300', excelColWidth:'40' },
       { text: 'Ins. Kategori', value: 'categoryInitial', divider: true, width: '120', excelColWidth:'12' },
       { text: 'Gudang', value: 'warehouseName', divider: true, width: '120', excelColWidth:'12' },
-      { text: 'Status', value: 'status', divider: true, width: '160', excelColWidth:'20' },
       { text: 'Qty', value: 'qty', align: 'right', divider: true, width: '100', excelColWidth:'20' },
       { text: 'Satuan', value: 'unitName', divider: true, width: '120', excelColWidth:'12' },
       { text: 'Harga Kotor', value: 'grossAmount', align: 'right', divider: true, width: '100', excelColWidth:'20', isCurrency: true },
