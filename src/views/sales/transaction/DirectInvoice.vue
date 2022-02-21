a8246c6b229c6324649b55fa1a316f046c617cb7
Galang Bilal Farchan <galang.bilal@sahassa.co.id>
2022-02-21 11:55:53 +0700

    Fix frontend final disc calculation

--- src/views/sales/transaction/DirectInvoice.vue
+++ src/views/sales/transaction/DirectInvoice.vue
@@ -1728,7 +1728,7 @@ export default {
       }
     },
     discPercentChange() {
-      this.data.finalDisc = this.data.dpp * (this.data.finalDiscPercent / 100)
+      this.data.finalDisc = (this.data.dpp + _sumBy(this.gridItem.data, 'totFDH')) * (this.data.finalDiscPercent / 100)
       this.calcGrandTotal()
     },
     discChange() {
