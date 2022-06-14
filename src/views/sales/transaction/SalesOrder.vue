8624405370150fde0efa11805dd6b9e35b615694
Galang Bilal Farchan <galang.bilal@sahassa.co.id>
2022-06-14 11:56:40 +0700

    Add calculation for discount in SO&DI

--- src/views/sales/transaction/SalesOrder.vue
+++ src/views/sales/transaction/SalesOrder.vue
@@ -1980,6 +1980,7 @@ export default {
       }
     },
     calcItemPrice(item, calcPrice = true) {
+      this.calcDisc(item)
       this.calcItemTax(item)
       item.total = item.qty * item.nettPrice
       item.totTax = item.qty * item.taxAmount
@@ -2100,6 +2101,39 @@ export default {
     },
     changeDate() {
       this.gridPromo.data = []
+    },
+    calcDisc(item) {
+      if (item.discPromo) {
+        for (let i = 0; i < item.discPromo.length; i++) {
+          if (i === 0) {
+            if (item.discPromo[i].promoMethod === 1 || item.discPromo[i].isPercentage) {
+              item.discPromo[i].nettPrice = item.unitPrice - (item.unitPrice * (item.discPromo[i].value / 100))
+            } else {
+              item.discPromo[i].nettPrice = item.unitPrice - item.discPromo[i].value
+            }
+            item.nettPrice = item.discPromo[i].nettPrice
+            item.disc = item.unitPrice - item.nettPrice
+          } else {
+            if (item.discPromo[i].promoMethod === 1 || item.discPromo[i].isPercentage) {
+              item.discPromo[i].nettPrice = item.nettPrice - (item.unitPrice * (item.discPromo[i].value / 100))
+            } else {
+              item.discPromo[i].nettPrice = item.nettPrice - item.discPromo[i].value
+            }
+            item.nettPrice = item.discPromo[i].nettPrice
+            item.disc = item.unitPrice - item.nettPrice
+          }
+          if (item.discPromo[i].isPercentage) {
+            item.discPromo[i].promoMethod = 1
+          } else {
+            item.discPromo[i].promoMethod = 2
+          }
+          if (item.discPromo[i].promoCode) {
+            item.discPromo[i].fromPromo = true
+          } else {
+            item.discPromo[i].fromPromo = false
+          }
+        }
+      }
     }
   }
 }
