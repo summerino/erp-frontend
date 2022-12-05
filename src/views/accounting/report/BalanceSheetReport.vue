aa7f7b81b14bff0977fe5c45c0afc41bfd0fc5f3
Galang Bilal Farchan <galang.bilal@sahassa.co.id>
2022-12-05 16:39:23 +0700

    Fix accounting report filter excel

--- src/views/accounting/report/BalanceSheetReport.vue
+++ src/views/accounting/report/BalanceSheetReport.vue
@@ -433,7 +433,7 @@ export default {
           }
           this.grid.columns = this.ledgerColumn
           this.grid.data = response.data
-          this.appendGLFilter()
+          //this.appendGLFilter()
         })
     },
     getDetail() {
@@ -455,7 +455,7 @@ export default {
           }
           this.grid.columns = this.detailColumn
           this.grid.data = response.data
-          this.appendDetailFilter()
+          //this.appendDetailFilter()
         })
     },
     showfilter() {
