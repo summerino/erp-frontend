793ffaf3502667977cf06bd44ac3195d821a2f8a
Galang Bilal Farchan <galang.bilal@sahassa.co.id>
2022-12-08 15:28:04 +0700

    disable sources option if data invoiced

--- src/views/sales/transaction/DeliveryOrder.vue
+++ src/views/sales/transaction/DeliveryOrder.vue
@@ -327,6 +327,7 @@
                           v-model="data.srcTrans"
                           :items="sources"
                           :rules="rules.required"
+                          :readonly="isInvoiced"
                           label="Sumber Transaksi"
                           item-text="name"
                           item-value="id"
@@ -986,6 +987,9 @@ export default {
     },
     isReturn() {
       return (this.data.srcTrans === 2)
+    },
+    isInvoiced() {
+      return (this.data.mark === 'INV')
     }
   },
 
