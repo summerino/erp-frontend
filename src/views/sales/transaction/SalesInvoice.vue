47bfa7ce07a7f3d513d35ba244154255773a629c
Galang Bilal Farchan <galang.bilal@sahassa.co.id>
2023-06-14 11:35:22 +0700

    Fix selection box in DO & SI

--- src/views/sales/transaction/SalesInvoice.vue
+++ src/views/sales/transaction/SalesInvoice.vue
@@ -185,6 +185,7 @@
         class="elevation-1"
         fixed-header
         show-select
+        @toggle-select-all="selectAllToggle"
       >
         <template v-slot:[`item.data-table-select`]="{ item, isSelected, select }">
           <v-simple-checkbox
@@ -1939,6 +1940,19 @@ export default {
       })
 
       this.siSDPData = result.data.tableData
+    },
+    selectAllToggle(props) {
+      if (this.selected.length !== this.grid.data.length  - this.grid.data.filter(x => x.mark.toUpperCase() === 'V').length) {
+        this.selected = []
+        const self = this
+        props.items.forEach(item => {
+          if (item.mark.toUpperCase() !== 'V') {
+            self.selected.push(item)
+          } 
+        })
+      } else {
+        this.selected = []
+      }
     }
   }
 }
